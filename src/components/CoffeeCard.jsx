defe464716b2096e12b616e48275057dce35ef78
Your Name <freelancermd82@gmail.com>
2024-09-12 09:06:02 +0600

    delete alart

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,10 +1,44 @@
+import { data } from 'autoprefixer';
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
 
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
 
 
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted',
+                                'Your Coffee has been deleted',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        });
+
+    }
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <figure>
@@ -13,7 +47,7 @@ const CoffeeCard = ({ coffee }) => {
                     alt="Movie" />
             </figure>
             <div className="flex justify-between w-full pr-4">
-                <div>
+                <div className='ps-4'>
                     <h2 className="card-title">Name: {name}</h2>
                     <p>{quantity}</p>
                     <p>{supplier}</p>
@@ -23,7 +57,9 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="join join-vertical space-y-4">
                         <button className="btn">View</button>
                         <button className="btn">Edit</button>
-                        <button className="btn">X</button>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn bg-orange-500">X</button>
                     </div>
                 </div>
             </div>
